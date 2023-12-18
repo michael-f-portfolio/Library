bc09ba93230a6e8989071fe3a89e4f335810d2a4
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2023-12-18 16:48:13 -0700

    Added confirm modal when removing a book

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const libraryBooks = [
-    new Book (1, "Meditations", "Marcus Aurelius", 190, false),
-    new Book (2, "Future Science", "Max Brockman", 247, false),
-    new Book (3, "The Journey of Socrates", "Dan Millman", 336, true)
+    new Book ("Meditations", "Marcus Aurelius", 190, false),
+    new Book ("Future Science", "Max Brockman", 247, false),
+    new Book ("The Journey of Socrates", "Dan Millman", 336, true)
 ];
 
-function Book(index, title, author, numberOfPages, hasBeenRead) {
-    this.index = index;
+function Book(title, author, numberOfPages, hasBeenRead) {
+    this.id = Math.floor((Math.random() * 1000000));
     this.title = title;
     this.author = author;
     this.pages = numberOfPages;
@@ -32,22 +32,21 @@ function addBookToLibrary() {
             newBookNumberOfPages = parseInt(input.lastElementChild.value);
         }
     });
-
     newBookHasBeenRead = hasBeenReadCheckbox.checked === true;
-
-    libraryBooks.push(new Book(libraryBooks.length, newBookTitle, newBookAuthor, newBookNumberOfPages, newBookHasBeenRead));
+    libraryBooks.push(new Book(newBookTitle, newBookAuthor, newBookNumberOfPages, newBookHasBeenRead));
     displayBooks();
 }
 
-function removeBookFromLibrary() {
-    libraryBooks.splice(this.getAttribute("id"), 1)
+function removeBookFromLibrary(bookId) {
+    const bookToRemoveIndex = libraryBooks.findIndex(book  => book.id === parseInt(bookId));
+    const removedBook = libraryBooks.splice(bookToRemoveIndex, 1);
+    console.log(`Removed: ${removedBook[0].title}`);
     displayBooks();
 }
 
 function displayBooks() {
     let booksContainer = document.querySelector(".display-books");
     booksContainer.innerHTML = "";
-    let bookIndex = 0;
     libraryBooks.forEach(book => {
         let newBookContainer = document.createElement("div");
         newBookContainer.className = "book";
@@ -81,13 +80,23 @@ function displayBooks() {
         newBookContainer.append(bookDetailsDiv);
         newBookContainer.append(bookStatsDiv);
 
-        // Add Book Actions
+        //// Add Book Actions
+        // Remove Book (Show confirm modal)
         let removeBookButton = document.createElement("button");
         removeBookButton.textContent = "Remove Book";
-        removeBookButton.setAttribute("id", bookIndex++);
+        removeBookButton.setAttribute("bookId", book.id);
 
-        removeBookButton.addEventListener("click", removeBookFromLibrary);
+        const removeBookDialog = document.querySelector("#removeBookDialog");
+        removeBookButton.addEventListener("click", (event) => {
+            console.log(event);
+            const bookId = event.target.getAttribute("bookId");
+            const removeBookTitle = document.querySelector("#removeBookTitle").querySelector("span");
+            removeBookTitle.textContent = libraryBooks.find(book => book.id === parseInt(bookId)).title;
+            removeBookTitle.setAttribute("bookId", bookId);
+            removeBookDialog.showModal();
+        });
 
+        // Toggle Read Status
         let toggleReadButton = document.createElement("button");
         toggleReadButton.textContent = "Toggle Read";
         toggleReadButton.addEventListener("click", book.toggleRead.bind(book));
@@ -105,20 +114,38 @@ function displayBooks() {
     });
 }
 
-const showButton = document.querySelector("#showAddBookDialog");
+// Show Add book dialog modal
+const showAddBookDialogButton = document.querySelector("#showAddBookDialog");
 const addBookDialog = document.querySelector("#addBookDialog");
-const confirmBtn = addBookDialog.querySelector("#confirmBtn");
-const addBookContainer = document.querySelector(".add-book-container");
-showButton.addEventListener("click", () => {
+const confirmAddButton = addBookDialog.querySelector("#confirmAddBtn");
+showAddBookDialogButton.addEventListener("click", () => {
     addBookDialog.showModal();
 });
-confirmBtn.addEventListener("click", (event) => {
+// Add to library and close dialog modal
+confirmAddButton.addEventListener("click", (event) => {
     event.preventDefault();
     addBookToLibrary();
     addBookDialog.close();
 });
+// Close dialog modal if a click happens anywhere but within the container div
 addBookDialog.addEventListener("click", () => addBookDialog.close());
+const addBookContainer = document.querySelector(".add-book-container");
 addBookContainer.addEventListener("click", (event) => event.stopPropagation());
+
+// Remove from library and close dialog modal
+const confirmRemoveButton = document.querySelector("#confirmRemoveBtn");
+const removeBookDialog = document.querySelector("#removeBookDialog");
+confirmRemoveButton.addEventListener("click", () => {
+    const removeBookTitle = document.querySelector("#removeBookTitle");
+    const bookToRemoveIndex = removeBookTitle.querySelector("span").getAttribute("bookId");
+    removeBookFromLibrary(bookToRemoveIndex);
+    removeBookDialog.close();
+});
+// Close Remove Dialog Modal
+const cancelRemoveButton = document.querySelector("#cancelRemoveBtn");
+cancelRemoveButton.addEventListener("click", () => {
+    removeBookDialog.close();
+});
 
 function app() {
     displayBooks();
